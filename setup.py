3ce80bec9fdc984bb8eb65c3accf92a700200b43
Chitransh-Kumar <kumar.chitransh1103@gmail.com>
2026-03-12 17:46:32 +0530

    Setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name= "Medical-Chatbot",
+    version= "0.1.0",
+    author= "Chitransh Kumar",
+    author_email= "chitranshhere11@gmail.com",
+    packages= find_packages(),
+    install_requires= []
+)
